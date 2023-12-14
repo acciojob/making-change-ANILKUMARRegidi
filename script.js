6fbe16e976876281d1299d25516e9c0c2e7be513
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2023-12-14 11:08:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 const makeChange = (c) => {
-  // your name here
+ const myMoney =
+	 { penny:'0.01';
+       nickels:'0.05';
+       dime:'0.10';
+       quarter:'0.25';}
+let change = 
+	{
+		p:0;
+		n:0;
+        d:0;
+        q:0;
+	}
+    while(c>0)
+		{
+		  if(c>myMoney.quarter)
+		  {
+			  change.q++;
+			  c -= myMoney.quarter;
+		  }
+			else if(c>myMoney.dime)
+			{
+				change.d++;
+				c -= myMoney.dime;
+			}
+			else if(c>myMoney.nickels)
+			{
+				change.nickels++;
+				c -= myMoney.nickels;
+			}
+			else
+			{
+				change.penny++;
+				c -= myMoney.penny;
+			}
+		}
+	return change;
 };
 
 // Do not the change the code below
