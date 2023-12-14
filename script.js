c30e84d55cf2319528340b3469716a7d0d40f57d
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2023-12-14 11:15:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,38 @@
-const makeChange = (c) => {
- const myMoney =
-	 { penny:'0.01';
-       nickels:'0.05';
-       dime:'0.10';
-       quarter:'0.25';}
-let change = 
-	{
-		p:0;
-		n:0;
-        d:0;
-        q:0;
-	}
-    while(c>0)
-		{
-		  if(c>myMoney.quarter)
-		  {
-			  change.q++;
-			  c -= myMoney.quarter;
-		  }
-			else if(c>myMoney.dime)
-			{
-				change.d++;
-				c -= myMoney.dime;
-			}
-			else if(c>myMoney.nickels)
-			{
-				change.nickels++;
-				c -= myMoney.nickels;
-			}
-			else
-			{
-				change.penny++;
-				c -= myMoney.penny;
-			}
-		}
-	return change;
-};
+ const makeChange = (c) => {
+  const myMoney = {
+    penny: 0.01,
+    nickel: 0.05,
+    dime: 0.10,
+    quarter: 0.25
+  };
 
-// Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+  let change = {
+    p: 0,
+    n: 0,
+    d: 0,
+    q: 0
+  };
+
+  while (c > 0) {
+    if (c >= myMoney.quarter) {
+      change.q++;
+      c -= myMoney.quarter;
+    } else if (c >= myMoney.dime) {
+      change.d++;
+      c -= myMoney.dime;
+    } else if (c >= myMoney.nickel) {
+      change.n++;
+      c -= myMoney.nickel;
+    } else {
+      change.p++;
+      c -= myMoney.penny;
+    }
+  }
+
+  return change;
+}
+
+// Example: Prompt the user to enter a value for 'c'
+const userInput = prompt("Enter c: ");
+const result = makeChange(userInput);
+alert(JSON.stringify(result));
